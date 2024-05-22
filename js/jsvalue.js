def2e21e83ce98cddd986318e7d2137d198d7e75
AleZ2024 <169025639+AleZ2024@users.noreply.github.com>
2024-05-21 23:23:52 -0300

    Update jsvalue.js
    modal login

--- js/jsvalue.js
+++ js/jsvalue.js
@@ -4,3 +4,20 @@ function scrollToTop() {
         behavior: "smooth"
     });
 } //Es para el btnInicio
+
+
+ //Es para el modal del login
+var loginButton = document.getElementById("loginButton");
+var loginModal = document.getElementById("loginModal");
+loginButton.addEventListener("click", function() {
+    loginModal.style.display = "block";
+});
+document.getElementById("closeButton").addEventListener("click", function() {
+    loginModal.style.display = "none";
+});
+window.addEventListener("click", function(event) {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+});
+
