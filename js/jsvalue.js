428d126f6cf91733e6cbec84d9da71fc1ad808ad
AleZ2024 <169025639+AleZ2024@users.noreply.github.com>
2024-05-21 23:58:06 -0300

    Update jsvalue.js

--- js/jsvalue.js
+++ js/jsvalue.js
@@ -2,21 +2,10 @@ function scrollToTop() {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
-    });
-} //Es para el btnInicio
+    });} //Es para el btnInicio
 
 
  //Es para el modal del login
-var loginButton = document.getElementById("Login"); 
-var loginModal = document.getElementById("loginModal");
-loginButton.addEventListener("click", function() {
-    loginModal.style.display = "block";
-});
-document.getElementById("closeButton").addEventListener("click", function() {
-    loginModal.style.display = "none";
-});
-window.addEventListener("click", function(event) {
-    if (event.target == loginModal) {
-        loginModal.style.display = "none";
-    }
-});
+document.getElementById("clearButton").addEventListener("click", function() {
+document.getElementById("loginForm").reset();});
+
